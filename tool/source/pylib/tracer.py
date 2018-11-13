ee3b877d054bb1bd425019a2b53c321a77853e31
liuxinyuan <liuxinyuan@ubuntu.ubuntu-domain>
2018-11-14 00:08:01 +0800

    commit

--- tool/source/pylib/tracer.py
+++ tool/source/pylib/tracer.py
@@ -82,6 +82,7 @@ def run(project,bugid,patch_no,tests,randoop_tests=[],tmp_tracefile='tmp_c'):
                 os.system('mv '+tmp_tracefile+' '+os.path.join(dir_path,'buggy','__'.join(test.split('::'))))
             else:
                 os.system('rm '+tmp_tracefile)
+                continue
 
         ret=os.system('timeout 90 defects4j test -n -t '+test+' -w  '+w_patched+jvmargs)
         if os.path.exists(tmp_tracefile):
@@ -105,6 +106,7 @@ def run(project,bugid,patch_no,tests,randoop_tests=[],tmp_tracefile='tmp_c'):
                 os.system('mv '+tmp_tracefile+' '+os.path.join(dir_path,'buggy','__'.join(test.split('::'))))
             else:
                 os.system('rm '+tmp_tracefile)
+                continue
         if(cmpl_flag):
             ret=os.system('timeout 90 defects4j test -s '+testfile+' -t '+Test_Case.strip()+' -w '+w_patched+jvmargs)
         else:
